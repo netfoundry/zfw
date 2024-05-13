8898858ee4ffcc4428a649b763f159ce5fcc10a2
r-caamano <robert.caamano@netfoundry.io>
2024-05-13 03:40:22 +0000

    reverted outbound_track

--- src/zfw_tc_outbound_track.c
+++ src/zfw_tc_outbound_track.c
@@ -58,7 +58,6 @@ struct bpf_event{
     __u8 tracking_code;
     unsigned char source[6];
     unsigned char dest[6];
-    char service_id[29];
 };
 
 /*Key to tcp_map and udp_map*/
@@ -294,7 +293,6 @@ int bpf_sk_splice(struct __sk_buff *skb){
         EGRESS,
         0,
         0,
-        {0},
         {0},
         {0}
      };
